827340fa29abfebdffcfd545cae8041f1e82bcfd
yaguch1 <128358148+yaguch1@users.noreply.github.com>
2023-03-28 15:26:17 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
@@ -20,3 +22,4 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
+
